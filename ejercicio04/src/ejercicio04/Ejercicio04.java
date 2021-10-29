652b603a600d1c9b34d00abb64499c378ffa0658
Jose Benitez} <jlbenitez4@utpl.edu.ec>
2021-10-29 11:47:50 -0500

    primer commit

---
+++ ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author SALAA
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int variable1 = 20;
+        int variable2 = 1;
+        int variable3 = 3;
+        int variable4 = 2;
+        
+        boolean resultado = variable1/(variable2+variable3)+variable4>variable3;
+        System.out.println(resultado);
+        
+        
+        
+        
+    }
+    
+}
--- ejercicio05/src/ejercicio05/Ejercicio05.java
+++ ejercicio05/src/ejercicio05/Ejercicio05.java
@@ -3,29 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio03;
+package ejercicio05;
 
 /**
  *
  * @author SALAA
  */
-public class Ejercicio03 {
+public class Ejercicio05 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here}
-        int variable1 = 10;
+        int variable1 = 2;
         int variable2 = 2;
-        int variable3 = 16;
-        int variable4 = 20;
-        int variable5 = 11;
-        int variable6 = 1;
-        double resultado = Math.pow(variable1, variable2) + Math.sqrt(variable3)
-                + variable4 / (variable5 + variable6);
+        int variable3 = 12;
+        int variable4 = 1;
+        int variable5 = 3;
+        double resultado = (Math.pow(variable1, variable2) + (variable3 - variable4))
+                / variable5;
         System.out.println(resultado);
-
     }
 
 }
